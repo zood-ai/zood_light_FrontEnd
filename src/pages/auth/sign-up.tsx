230df1699150da9187de86356606d02b4c99590d
ayman-zood <20004@zood.ai>
2024-11-20 02:56:44 +0200

    signup structure

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,64 +1,301 @@
-import { Card } from '@/components/ui/card'
-import { SignUpForm } from './components/sign-up-form'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import axiosInstance from '@/api/interceptors';
+import { Button } from '@/components/custom/button';
+import { CheckboxWithText } from '@/components/custom/CheckboxWithText';
 
 export default function SignUp() {
+  const [formState, setFormState] = useState({
+    name: '',
+    plan: '',
+    email: '',
+    phone: '',
+    password: '',
+    confirmPassword: '',
+    business_name: '',
+    country: '',
+    city: '',
+    district: '',
+    streetName: '',
+    postalCode: '',
+    business_type_id: '',
+    business_location_id: '',
+    tradeRegister: null,
+    emailAlert: false,
+  });
+
+  const handleChange = (key: string, value: any) => {
+    setFormState((prevState) => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const myFormData = new FormData();
+    myFormData.append('name', formState.name);
+    myFormData.append('email', formState.email);
+    myFormData.append('password', formState.name);
+    myFormData.append('business_name', formState.business_name);
+    myFormData.append('business_type_id', formState.business_type_id);
+    myFormData.append('business_location_id', formState.business_location_id);
+    myFormData.append('phone', formState.phone);
+
+    const res = await axiosInstance.post('auth/Register', myFormData);
+    console.log(res.data);
+  };
   return (
-    <>
-      <div className='container grid h-svh flex-col items-center justify-center bg-primary-foreground lg:max-w-none lg:px-0'>
-        <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8'>
-          <div className='mb-4 flex items-center justify-center'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='mr-2 h-6 w-6'
-            >
-              <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-            </svg>
-            <h1 className='text-xl font-medium'>Shadcn Admin</h1>
-          </div>
-          <Card className='p-6'>
-            <div className='mb-2 flex flex-col space-y-2 text-left'>
-              <h1 className='text-lg font-semibold tracking-tight'>
-                Create an account
-              </h1>
-              <p className='text-sm text-muted-foreground'>
-                Enter your email and password to create an account. <br />
-                Already have an account?{' '}
-                <Link
-                  to='/sign-in'
-                  className='underline underline-offset-4 hover:text-primary'
-                >
-                  Sign In
-                </Link>
-              </p>
+    <div className="flex justify-center py-10">
+      <form onSubmit={handleSubmit}>
+        <div className="mx-2 my-4">
+          <div dir="rtl" className="flex gap-6 mb-6">
+            <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+              <Label className="align-right" htmlFor="name">
+                الاسم
+              </Label>
+              <Input
+                className=""
+                type="text"
+                id="name"
+                value={formState.name}
+                onChange={(e) => handleChange('name', e.target.value)}
+              />
             </div>
-            <SignUpForm />
-            <p className='mt-4 px-8 text-center text-sm text-muted-foreground'>
-              By creating an account, you agree to our{' '}
-              <a
-                href='/terms'
-                className='underline underline-offset-4 hover:text-primary'
+            <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+              <Label className="align-right" htmlFor="plan">
+                الخطة
+              </Label>
+              <Select
+                dir="rtl"
+                onValueChange={(value) => handleChange('plan', value)}
               >
-                Terms of Service
-              </a>{' '}
-              and{' '}
-              <a
-                href='/privacy'
-                className='underline underline-offset-4 hover:text-primary'
-              >
-                Privacy Policy
-              </a>
-              .
-            </p>
-          </Card>
+                <SelectTrigger className="w-[327px]">
+                  <SelectValue placeholder="" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="apple">Apple</SelectItem>
+                    <SelectItem value="banana">Banana</SelectItem>
+                    <SelectItem value="blueberry">Blueberry</SelectItem>
+                    <SelectItem value="grapes">Grapes</SelectItem>
+                    <SelectItem value="pineapple">Pineapple</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+          <div dir="rtl" className="flex gap-6 mb-6">
+            <div className="flex gap-6">
+              <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+                <Label className="align-right" htmlFor="email">
+                  عنوان البريد الالكتروني
+                </Label>
+                <Input
+                  type="email"
+                  id="email"
+                  value={formState.email}
+                  onChange={(e) => handleChange('email', e.target.value)}
+                />
+              </div>
+              <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+                <Label className="align-right" htmlFor="phone">
+                  رقم التليفون
+                </Label>
+                <Input
+                  type="text"
+                  id="phone"
+                  value={formState.phone}
+                  onChange={(e) => handleChange('phone', e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+          <div dir="rtl" className="flex gap-6 mb-6">
+            <div className="flex gap-6">
+              <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+                <Label className="align-right" htmlFor="password">
+                  الرقم السري
+                </Label>
+                <Input
+                  type="password"
+                  id="password"
+                  value={formState.password}
+                  onChange={(e) => handleChange('password', e.target.value)}
+                />
+              </div>
+              <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+                <Label className="align-right" htmlFor="confirmPassword">
+                  تاكيد الرقم السري
+                </Label>
+                <Input
+                  type="password"
+                  id="confirmPassword"
+                  value={formState.confirmPassword}
+                  onChange={(e) =>
+                    handleChange('confirmPassword', e.target.value)
+                  }
+                />
+              </div>
+            </div>
+          </div>
+          <div dir="rtl" className="flex gap-6 mb-6">
+            <div className="flex gap-6">
+              <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+                <Label className="align-right" htmlFor="business_name">
+                  اسم المتجر
+                </Label>
+                <Input
+                  type="text"
+                  id="business_name"
+                  value={formState.business_name}
+                  onChange={(e) =>
+                    handleChange('business_name', e.target.value)
+                  }
+                />
+              </div>
+              <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+                <Label className="align-right" htmlFor="plan">
+                  نوع المتجر
+                </Label>
+                <Select
+                  dir="rtl"
+                  onValueChange={(value) => handleChange('plan', value)}
+                >
+                  <SelectTrigger className="w-[327px]">
+                    <SelectValue placeholder="" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="ًWorkshop">ًWorkshop</SelectItem>
+                      <SelectItem value="business">Business</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </div>
+          <div dir="rtl" className="flex gap-6 mb-6">
+            <div className="flex gap-6">
+              <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+                <Label className="align-right" htmlFor="country">
+                  الدولة
+                </Label>
+                <Input
+                  type="text"
+                  id="country"
+                  value={formState.country}
+                  onChange={(e) => handleChange('country', e.target.value)}
+                />
+              </div>
+              <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+                <Label className="align-right" htmlFor="city">
+                  المدينة
+                </Label>
+                <Input
+                  type="text"
+                  id="city"
+                  value={formState.city}
+                  onChange={(e) => handleChange('city', e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+          <div dir="rtl" className="flex gap-6 mb-6">
+            <div className="flex gap-6">
+              <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+                <Label className="align-right" htmlFor="district">
+                  الحي
+                </Label>
+                <Input
+                  type="text"
+                  id="district"
+                  value={formState.district}
+                  onChange={(e) => handleChange('district', e.target.value)}
+                />
+              </div>
+              <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+                <Label className="align-right" htmlFor="streetName">
+                  اسم الشارع
+                </Label>
+                <Input
+                  type="text"
+                  id="streetName"
+                  value={formState.streetName}
+                  onChange={(e) => handleChange('streetName', e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+          <div dir="rtl" className="flex gap-6 mb-6">
+            <div className="flex">
+              <div dir="rtl" className="grid max-w-sm items-center gap-1.5">
+                <Label className="align-right" htmlFor="postalCode">
+                  رمز بريدي
+                </Label>
+                <Input
+                  type="text"
+                  id="postalCode"
+                  value={formState.postalCode}
+                  onChange={(e) => handleChange('postalCode', e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <div dir="rtl">
+              <div className="w-[327px]">
+                <div className="flex flex-col items-start self-start mt-4 max-w-full text-sm text-left w-[223px]">
+                  <div className="font-medium text-zinc-500">
+                    تحميل السجل التجاري
+                  </div>
+                  <div className="flex gap-2   max-w-full items-center">
+                    <label className="flex flex-1 justify-center items-center font-semibold bg-gray-200 rounded border border-solid border-zinc-300 w-[117px] h-[39px] text-mainText cursor-pointer">
+                      <span className="text-[14px]">اختر الملف</span>
+                      <input
+                        type="file"
+                        accept="image/*"
+                        className="hidden"
+                        onChange={(e) =>
+                          handleChange(
+                            'tradeRegister',
+                            e.target.files?.[0] || null
+                          )
+                        }
+                      />
+                    </label>
+                  </div>
+                  <span className="text-gray-500 text-md">
+                    {formState.tradeRegister?.name || 'no file chosen'}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div dir="rtl">
+              <CheckboxWithText
+                className=""
+                label="ارسال تنبيه للبريد الالكتروني"
+                checked={formState.emailAlert}
+                onChange={(e) => handleChange('emailAlert', e)}
+              />
+            </div>
+            <div dir="rtl">
+              <Button type="submit" className="px-4 py-2 w-[150px] text-sm">
+                اضافة
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
-    </>
-  )
+      </form>
+    </div>
+  );
 }
